dd59492d1860de1a31e7e8d616b61542796ac22d
aviranaviv <aviranaviv@gmail.com>
2026-03-20 16:57:41 +0200

    add git ignore

--- first-script.js
+++ first-script.js
@@ -2,8 +2,9 @@ import http from 'k6/http';
 
 export const options = {
     vus: 10,
-    duration: '60s'
+    duration: '10s'
 }
+
 export default function () {
     http.get('https://quickpizza.grafana.com/test.k6.io/')
 }
