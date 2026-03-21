45e911d0b688e9d39c6b2f6e9e95f6fa501e27a5
aviranaviv <aviranaviv@gmail.com>
2026-03-21 12:25:18 +0200

    add example of load and smoke tests

--- smoke-test.js
+++ smoke-test.js
@@ -2,11 +2,15 @@ import http from 'k6/http';
 import { sleep } from 'k6'
 
 export const options = {
-    vus: 10,
+    vus: 20,
     duration: '10s'
 }
 
 export default function () {
     http.get('https://quickpizza.grafana.com/test.k6.io/')
     sleep(1)
+    http.get('https://quickpizza.grafana.com/contacts.php')
+    sleep(2)
+    http.get('https://quickpizza.grafana.com/news.php')
+    sleep(2)
 }
