7e2925079847f5a9608600fbb140170cbb565cb0
Aviran Aviv <aviranaviv@gmail.com>
2026-04-17 15:42:59 +0300

    add new validation

--- tests/http-requests/example-of-assertions.js
+++ tests/http-requests/example-of-assertions.js
@@ -1,13 +1,15 @@
 import http from "k6/http";
 import {check, sleep} from "k6";
-import { expect } from "https://jslib.k6.io/k6-testing/0.3.0/index.js";
 
 export const options = {
+    vus: 10,
+    duration: '5s',
     thresholds: {
-        http_req_duration: ['p(95)<200'],
-        http_req_failed: ['rate<0.01'],
-        iterations: ['count<=50'],
-        data_received: ['count<=' + 309 * 1024]
+        http_req_duration: ['p(95) < 200', 'max < 2000'],
+        http_req_failed: ['rate < 0.01'],
+        http_reqs: ['count >= 20', 'rate >= 4'],
+        checks: ['rate >= 0.99'],
+        vus: ['value > 9'],
     }
 }
 
@@ -18,6 +20,5 @@ export default function () {
         'Status code is 200': res.status === 200,
         'The body include some text': res.body.toString().includes('Note that this is a shared testing environment ')
     });
-
-    expect(res.status, 'response status').toBe(200);
+    sleep(2);
 }
